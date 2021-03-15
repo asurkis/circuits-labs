60ebaff4f24b988197b317b770e52d58d7ac4157
Anton Surkis <antonsurkis@gmail.com>
2021-03-15 22:12:50 +0300

    Local images, added algorithm description

--- Lab2/mult.v
+++ Lab2/mult.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 
 module mult (
-    input clk_i,
-    input rst_i ,
-    input [7 : 0] a_bi,
-    input [7 : 0] b_bi,
-    output busy_o,
-    output reg [15 : 0] y_bo
+	input clk_i,
+	input rst_i ,
+	input [7 : 0] a_bi,
+	input [7 : 0] b_bi,
+	output busy_o,
+	output reg [15 : 0] y_bo
 );
 
 localparam IDLE = 1'b0;
@@ -26,25 +26,25 @@ assign end_step = (ctr == 3'h7);
 assign busy_o = rst_i | state;
 
 always @(posedge clk_i)
-    if (rst_i) begin
-        ctr <= 0;
-        part_res <= 0;
-        y_bo <= 0;
-        state <= WORK;
-        a <= a_bi;
-        b <= b_bi;
-    end else begin
-        case (state)
-            IDLE: y_bo <= part_res;
-            WORK:
-                begin
-                    if (end_step) begin
-                        state <= IDLE;
-                        y_bo <= part_res;
-                    end
-                    part_res <= part_res + shifted_part_sum;
-                    ctr <= ctr + 1;
-                end
-        endcase
-    end
+	if (rst_i) begin
+		ctr <= 0;
+		part_res <= 0;
+		y_bo <= 0;
+		state <= WORK;
+		a <= a_bi;
+		b <= b_bi;
+	end else begin
+		case (state)
+			IDLE: y_bo <= part_res;
+			WORK:
+				begin
+					if (end_step) begin
+						state <= IDLE;
+						y_bo <= part_res;
+					end
+					part_res <= part_res + shifted_part_sum;
+					ctr <= ctr + 1;
+				end
+		endcase
+	end
 endmodule
