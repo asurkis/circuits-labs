ec47d52ba3ee4cadaa8789f9abcfebe309b9cc03
rbetik12 <mr.prikota@gmail.com>
2021-03-26 22:16:33 +0300

    updated accelerator code

--- Lab2/accelerator.v
+++ Lab2/accelerator.v
@@ -14,7 +14,7 @@ reg [8:0] x;
 reg [15:0] t;
 reg [7:0] r;
 reg [7:0] m;
-reg [3:0] state;
+reg [3:0] state, state_next;
 reg [7:0] mult_i1;
 reg [7:0] mult_i2;
 wire [15:0] mult_out;
@@ -61,125 +61,125 @@ localparam STATE7 = 4'b0111;
 localparam STATE8 = 4'b1000;
 localparam STATE9 = 4'b1001;
 localparam STATE10 = 4'b1010;
-/* localparam STATE11 = 4'b1011;
+localparam STATE11 = 4'b1011;
 localparam STATE12 = 4'b1100;
 localparam STATE13 = 4'b1101;
 localparam STATE14 = 4'b1110;
-localparam STATE15 = 4'b1111; */
+localparam STATE15 = 4'b1111;
 
 assign busy_out = rst_i | |state;
 assign y_out = r;
 
+always @(posedge clk_i) 
+    if (rst_i) begin
+        state <= STATE1;
+    end else begin
+        state <= state_next;
+    end
+    
+always @* begin
+        case(state)
+            STATE0: state_next = STATE0;
+            STATE1: state_next = |m ? STATE2 : STATE5;
+            STATE2: state_next = mult_busy ? STATE2 : STATE3;
+            STATE3: state_next = mult_busy ? STATE3 : STATE4;
+            STATE4: state_next = STATE1;
+            STATE5: state_next = STATE6;
+            STATE6: state_next = STATE7;
+            STATE7: state_next = |m ? STATE8 : STATE0;
+            STATE8: state_next = x >= t ? STATE9 : STATE11;
+            STATE9: state_next = STATE10;
+            STATE10: state_next = STATE11;
+            STATE11: state_next = STATE7;
+        endcase
+end
+
+always @(posedge rst_i) begin
+    
+end
+
 always @(posedge clk_i) begin
-	if (rst_i) begin
-		a <= a_in;
-		x <= b_in;
-		t <= 0;
-		r <= 4;
-		m <= 4;
-		state <= STATE1;
-		mult_reset <= 0;
-	end else begin
-		case (state)
-			STATE0:
-				begin
-				end
-			STATE1:
-				begin
-					if (|m) begin
-						mult_reset <= 1;
-						mult_i1 <= r;
-						mult_i2 <= r;
-						state <= STATE2;
-					end else begin
-						state <= STATE5;
-					end
-				end
-			STATE2:
-				begin
-					if (mult_busy) begin
-						mult_reset <= 0;
-					end else begin
-						mult_reset <= 1;
-						mult_i1 <= mult_out;
-						mult_i2 <= r;
-						state <= STATE3;
-					end
-				end
-			STATE3:
-				begin
-					mult_reset <= 0;
-					if (!mult_busy) begin
-						if (x < mult_out) begin
-							sub_i1 = r;
-							sub_i2 = m;
-							#10
-							r = sub_o;
-						end else if (x > mult_out) begin
-						    sum_i1 = r;
-						    sum_i2 = m;
-						    #10
-						    r = sum_o;
-						end else begin
-							m <= 0;
-						end
-						state <= STATE4;
-					end
-				end
-			STATE4:
-				begin
-					m <= m >> 1;
-					state <= STATE1;
-				end
-			STATE5:
-				begin
-					sum_i1 = a;
-				    sum_i2 = r;
-					#10
-					x = sum_o;
-					m <= 64;
-					state <= STATE6;
-				end
-			STATE6:
-				begin
-					r <= 0;
-					state <= STATE7;
-				end
-			STATE7:
-				begin
-					t <= r | m;
-					state <= STATE8;
-					if (|m) begin
-						state <= STATE8;
-					end else begin
-						state <= STATE0;
-					end
-				end
-			STATE8:
-				begin
-					r <= r >> 1;
-					if (x >= t) begin
-						state <= STATE9;
-					end else begin
-						state <= STATE10;
-					end
-				end
-			STATE9:
-				begin
-					sub_i1 = x;
-					sub_i2 = t;
-					#10
-					r = sub_o;
-					r <= r | m;
-					state <= STATE10;
-				end
-			STATE10:
-				begin
-					m <= m >> 2;
-					state <= STATE7;
-				end
-		endcase
-	end
+    if (rst_i) begin
+        a <= a_in;
+        x <= b_in;
+        t <= 0;
+        r <= 4;
+        m <= 4;
+        mult_reset <= 0;
+    end else begin
+    case (state)
+        STATE0:
+            begin
+            end
+        STATE1:
+            begin
+                if (|m) begin
+                    mult_reset <= 1;
+                    mult_i1 <= r;
+                    mult_i2 <= r;
+                end
+            end
+        STATE2:
+            begin
+                if (mult_busy) begin
+                    mult_reset <= 0;
+                end else begin
+                    mult_reset <= 1;
+                    mult_i1 <= mult_out;
+                    mult_i2 <= r;
+                end
+            end
+        STATE3:
+            begin
+                mult_reset <= 0;
+                if (!mult_busy) begin
+                    sub_i1 <= r;
+                    sum_i1 <= r;
+                    if (x == mult_out) begin
+                        m <= 0;
+                        sum_i2 <= 0;
+                        sub_i2 <= 0;
+                    end else begin
+                        sub_i2 <= m;
+                        sum_i2 <= m;
+                    end
+                end
+            end
+        STATE4:
+             begin
+                 if (x <= mult_out) begin
+                    r <= sub_o;
+                    m <= m >> 1;
+                 end else begin
+                    r <= sum_o;
+                    if (m > 1) begin
+                        m <= m >> 1;
+                    end
+                 end
+             end
+        STATE5:
+            begin
+                sum_i1 <= a;
+                sum_i2 <= r;
+                m <= 64;
+            end
+        STATE6:
+         begin
+            x <= sum_o;
+            r <= 0;
+          end
+        STATE7: t <= r | m;
+        STATE8: r <= r >> 1;
+        STATE9:
+            begin
+                sub_i1 <= x;
+                sub_i2 <= t;
+                r <= r | m;
+            end
+        STATE10: x <= sub_o;
+        STATE11: m <= m >> 2;
+    endcase
+    end
 end
 
 endmodule
